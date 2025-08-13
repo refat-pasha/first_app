080efedb76e694acf55d9f9c9f7de8a67b50cc36
turtle <141565698+refat-pasha@users.noreply.github.com>
2025-08-13 22:31:49 +0600

    learning app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/splash_screen.dart';
+import 'package:first_app/screens/appbar.dart';
 
 void main() {
   runApp(const MyFirstApp());
@@ -12,8 +13,8 @@ class MyFirstApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My First Flutter App',
-      theme: ThemeData.dark(),
-      home: const SplashScreen(),
+      //theme: ThemeData.dark(),
+      home: const appbar(),
       debugShowCheckedModeBanner: false,
     );
   }
---
+++ lib/screens/appbar.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'floating_action_button.dart';
+
+void main(List<String> args) {}
+
+class appbar extends StatelessWidget {
+  const appbar({super.key});
+
+  // Function to show a SnackBar message
+  MySnackBar(String message, BuildContext context) {
+    return ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Things to learn(appbar)"),
+        titleSpacing: 10,
+        centerTitle: false,
+        toolbarHeight: 60,
+        toolbarOpacity: 1,
+        elevation: 0,
+        backgroundColor: Colors.green,
+        actions: [
+          IconButton(
+            onPressed: () {
+              MySnackBar("this is message", context);
+            },
+            icon: Icon(Icons.comment),
+          ),
+          IconButton(
+            onPressed: () {
+              MySnackBar("this is search", context);
+            },
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {
+              MySnackBar("this is setting", context);
+            },
+            icon: Icon(Icons.settings),
+          ),
+          IconButton(
+            onPressed: () {
+              MySnackBar("this is person", context);
+            },
+            icon: Icon(Icons.person),
+          ),
+          IconButton(
+            onPressed: () {
+              MySnackBar("going to floating action page", context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const floatingActionB(),
+                ),
+              );
+            },
+            icon: const Icon(Icons.next_plan),
+          ),
+        ],
+      ),
+    );
+  }
+}
