4795ea7769015a1bbc9659e3f70baee533d653c6
turtle <141565698+refat-pasha@users.noreply.github.com>
2025-08-14 23:10:02 +0600

    learning list view

---
+++ lib/screens/dynamic_ListView.dart
@@ -0,0 +1,116 @@
+import 'package:first_app/screens/simple_form.dart';
+import 'package:flutter/material.dart';
+import 'dynamic_GridView.dart';
+
+class ListViewPage extends StatelessWidget {
+  const ListViewPage({super.key});
+
+  void mySnackBar(String message, BuildContext context) {
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  final List<Map<String, String>> myItems = const [
+    {
+      "img": "assets/images/luffy.png",
+      "title": "Monkey D. Luffy",
+      "subtitle": "Captain of the Straw Hat Pirates",
+    },
+    {
+      "img": "assets/images/luffy2.png",
+      "title": "Monkey D. Luffy",
+      "subtitle": "Captain of the Straw Hat Pirates",
+    },
+    {
+      "img": "assets/images/luffy3.png",
+      "title": "Monkey D. Luffy",
+      "subtitle": "Captain of the Straw Hat Pirates",
+    },
+    {
+      "img": "assets/images/dragon.png",
+      "title": "Monkey D. Dragon",
+      "subtitle": "Leader of the Revolutionary Army",
+    },
+    {
+      "img": "assets/images/luffy.png",
+      "title": "Monkey D. Luffy",
+      "subtitle": "Captain of the Straw Hat Pirates",
+    },
+    {
+      "img": "assets/images/luffy2.png",
+      "title": "Monkey D. Luffy",
+      "subtitle": "Captain of the Straw Hat Pirates",
+    },
+    {
+      "img": "assets/images/luffy3.png",
+      "title": "Monkey D. Luffy",
+      "subtitle": "Captain of the Straw Hat Pirates",
+    },
+    {
+      "img": "assets/images/dragon.png",
+      "title": "Monkey D. Dragon",
+      "subtitle": "Leader of the Revolutionary Army",
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("things to learn (dynamic ListView)"),
+        titleSpacing: 10,
+        centerTitle: false,
+        toolbarHeight: 60,
+        toolbarOpacity: 1,
+        elevation: 0,
+        backgroundColor: Colors.green,
+        actions: [
+          IconButton(
+            onPressed: () {
+              mySnackBar("going to previous page", context);
+              // If intent is to go back, prefer Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const simple_form()),
+              );
+            },
+            icon: const Icon(Icons.arrow_back_ios_new_outlined),
+          ),
+          IconButton(
+            onPressed: () {
+              mySnackBar("this is next page loading", context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) =>  GridViewPage()),
+              );
+            },
+            icon: const Icon(Icons.next_plan),
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: myItems.length,
+        itemBuilder: (context, index) {
+          final item = myItems[index];
+          return GestureDetector(
+            onTap: () {
+              mySnackBar(item["title"] ?? "No title", context);
+            },
+            child: Container(
+              margin: const EdgeInsets.all(10),
+              width: double.infinity,
+              height: 250,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.black12,
+              ),
+              clipBehavior: Clip.antiAlias,
+              child: Image.asset(item["img"]!, fit: BoxFit.fill),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
